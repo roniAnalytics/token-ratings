c0bd3a869cc181c7b1c1442994da3c0a42e4c6a1
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-06-06 20:58:53 +0530

    feat: landing and bento

---
+++ scripts/pdf.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const openPdfBtn1 = document.getElementById("open-pdf-btn1");
+  const openPdfBtn2 = document.getElementById("open-pdf-btn2");
+  const openPdfBtn3 = document.getElementById("open-pdf-btn3");
+  const frame = document.getElementById("pdf-viewer");
+  const pdfPopup = document.getElementById("pdf-popup-content");
+  const pdfSection = document.getElementById("pdf-section");
+  const closePdfBtn = document.getElementById("close-pdf-btn");
+
+
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+  
+
+  openPdfBtn1.addEventListener("click", () => {
+    pdfPopup.style.display = "block";
+    frame.src = "./assets/sample.pdf";
+    pdfSection.style.backdropFilter = "blur(10px)";
+    // document.body.classList.add("noscroll");
+  });
+  openPdfBtn2.addEventListener("click", () => {
+    pdfPopup.style.display = "block";
+    frame.src = "./assets/file.pdf";
+    // document.body.classList.add("noscroll");
+  });
+  openPdfBtn3.addEventListener("click", () => {
+    pdfPopup.style.display = "block";
+    frame.src = "./assets/sample3.pdf";
+    // document.body.classList.add("noscroll");
+  });
+  closePdfBtn.addEventListener("click", () => {
+    pdfPopup.style.display = "none";
+    frame.src = "";
+    // document.body.classList.remove("noscroll");
+  });
+});
